beb5f7050618bbdab107f0e4221e6913f066e7b1
Administrator <Administrator@WIN-R0G8CJNRPRK>
2018-04-10 17:11:25 +0800

    价格

---
+++ TI_Solution_For_SCA/SCA/系统扩展/收货草稿单扩展/TI_Z000D.vb
@@ -0,0 +1,176 @@
+﻿Option Strict Off
+Option Explicit On
+Imports SAPbouiCOM
+Imports System.IO
+Imports Microsoft.Office.Interop.Excel
+Imports System.Runtime.InteropServices
+
+Public NotInheritable Class TI_Z000D
+    Inherits FormBase
+    Private ioDbds_DLN1, ioDbds_ODLN As DBDataSource
+    Private ioTempSql As SAPbouiCOM.DataTable
+    Private ioMtx_10 As Matrix
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function GetWindowThreadProcessId(
+    ByVal handle As Integer,
+    <Out()> ByRef processId As Integer) As Integer
+    End Function
+
+
+    Private Sub TI_Z0005_ItemEvent(FormUID As String, ByRef pVal As ItemEvent, ByRef BubbleEvent As Boolean) Handles Me.ItemEvent
+        Select Case pVal.EventType
+            Case BoEventTypes.et_FORM_LOAD
+                If pVal.BeforeAction Then
+                    ioTempSql = MyForm.DataSources.DataTables.Add("TempSql")
+                    '添加打印导出EXCEL的按钮
+                    Dim loItem, loItemChoose As Item
+                    loItem = MyForm.Items.Add("Export", BoFormItemTypes.it_BUTTON)
+                    Dim loBtn_Create1 As Item
+                    Dim loBtn_Export As SAPbouiCOM.Button
+                    loBtn_Create1 = MyForm.Items.Item("10000330")
+                    'loItem3 = MyForm.Items.Item("10000329")
+
+                    loItem.Left = loBtn_Create1.Left - 100 - 5
+                    loItem.Width = 100
+                    loItem.Top = loBtn_Create1.Top
+                    loItem.Height = loBtn_Create1.Height
+                    loItem.LinkTo = "10000330"
+                    loBtn_Export = loItem.Specific
+                    loBtn_Export.Caption = "打印"
+
+                    '添加打印选项
+                    loItemChoose = MyForm.Items.Add("ChooseList", BoFormItemTypes.it_COMBO_BOX)
+                    Dim loCmb_Chooselist As SAPbouiCOM.ComboBox
+                    loItemChoose.Left = loItem.Left - 100 - 5
+                    loItemChoose.Width = 100
+                    loItemChoose.Top = loItem.Top
+                    loItemChoose.Height = loItem.Height
+                    loItemChoose.AffectsFormMode = False
+                    loItemChoose.LinkTo = "Export"
+                    loCmb_Chooselist = loItemChoose.Specific
+                    Dim lsTempName As String
+                    Dim lsSQL As String = "select T10.U_TempPath,T11.U_Template,T11.U_PrintName,T11.U_PageSize,T11.U_TempName " &
+                                            "   from [@ti_z0010] T10 inner join [@ti_z0011] T11 On t10.Code=t11.code  " &
+                                              " where T10.code ='OPDN' and isnull(T11.U_TempName,'') <>'' "
+                    ioTempSql.ExecuteQuery(lsSQL)
+                    For i As Integer = 0 To ioTempSql.Rows.Count - 1
+                        lsTempName = ioTempSql.GetValue("U_TempName", i)
+                        loCmb_Chooselist.ValidValues.Add(lsTempName, lsTempName)
+                    Next
+                    loCmb_Chooselist.Select("仓库提货单", BoSearchKey.psk_ByValue)
+                End If
+            Case BoEventTypes.et_ITEM_PRESSED
+                '按导出EXCEL 时将交货单的数据导出到EXCEL
+                If Not pVal.Before_Action And pVal.ItemUID = "Export" Then
+                    'If MyForm.Mode <> BoFormMode.fm_OK_MODE Then
+                    '    MyApplication.MessageBox("只有在确认模式下才能打印!", "确定", "取消")
+                    '    Return
+                    'End If
+
+                    Dim loCmb_Choose As SAPbouiCOM.ComboBox
+                    Dim lsServicePath, lsTempEXCEL, lsPrintName, lsPageSize As String
+                    Dim loItem = MyForm.Items.Item("ChooseList")
+                    loCmb_Choose = loItem.Specific
+                    Dim lsTempName As String = loCmb_Choose.Selected.Value   '打印模板名称
+                    If lsTempName = "" Then
+                        Return
+                        BubbleEvent = False
+                    End If
+
+                    Dim lsSQL As String = "select T10.U_TempPath,T11.U_Template,T11.U_PrintName,T11.U_PageSize " &
+                                              "   from [@ti_z0010] T10 inner join [@ti_z0011] T11 On t10.Code=t11.code  " &
+                                                " where T10.code ='OPDN' and T11.U_tempname='" & lsTempName & "'"
+                    ioTempSql.ExecuteQuery(lsSQL)
+                    If ioTempSql.Rows.Count > 0 Then
+                        lsServicePath = ioTempSql.GetValue("U_TempPath", 0)
+                        lsTempEXCEL = ioTempSql.GetValue("U_Template", 0)
+                        lsPrintName = ioTempSql.GetValue("U_PrintName", 0)
+                        lsPageSize = ioTempSql.GetValue("U_PageSize", 0)
+                    End If
+                    Dim lsXML As String = MyForm.GetAsXML
+                    Dim liDocEntry As Integer = MyForm.DataSources.DBDataSources.Item(0).GetValue("DocEntry", 0)
+                    '查看该单据是否已经打印，如果已经打印，只有可以重复打印的人才可以打印，否则不能再次打印
+                    Dim liPrintedCopy As Integer = 0
+                    Dim lsRepeatSQL As String = ""
+                    Dim liRepeatCopy As Integer = 0
+                    Dim lsRepeatUser As String = ""
+                    Dim lsSQLPrinted As String = ""
+                    Dim lsPrintedUser, lsPrintedDatetime As String  '已重复打印用户，已重复打印时间
+                    lsPrintedUser = ""
+                    lsPrintedDatetime = ""
+
+                    Dim lsTargetFile As String = lsServicePath + lsTempEXCEL
+                    Dim lsU_Printer As String = lsPrintName  '打印机名称
+                    Dim lsU_PsizeID As String = lsPageSize   '打印机纸张
+                    Dim lsSourcePath As String  '原始表格路径
+                    lsSourcePath = lsPageSize
+
+                    '打开Excel
+                    Dim oExcelApp As Microsoft.Office.Interop.Excel.Application
+                    oExcelApp = New Microsoft.Office.Interop.Excel.Application
+                    Dim hwnd As Integer = CInt(oExcelApp.Hwnd)
+                    Dim processid As Integer
+                    GetWindowThreadProcessId(hwnd, processid)
+
+                    Dim m_objBooks As Microsoft.Office.Interop.Excel.Workbooks
+                    Dim m_objBook As Microsoft.Office.Interop.Excel.Workbook
+                    Dim m_objSheets As Microsoft.Office.Interop.Excel.Sheets
+                    Dim m_objSheet As Microsoft.Office.Interop.Excel.Worksheet
+
+                    ' File.Copy(lsSourcePath, lsTargetFile, True)
+
+                    oExcelApp.Visible = True
+                    oExcelApp.DisplayAlerts = False
+                    m_objBooks = oExcelApp.Workbooks
+                    m_objBook = m_objBooks.Open(lsTargetFile)
+
+                    m_objSheets = m_objBook.Worksheets
+                    m_objSheet = m_objSheets.Item(1) '定位第一张表
+                    m_objSheet.Activate()
+
+                    '设置打印机，纸张
+                    '检查打印机是否正确
+                    Dim doc As System.Drawing.Printing.PrintDocument = New System.Drawing.Printing.PrintDocument()
+                    doc.PrinterSettings.PrinterName = lsU_Printer
+                    doc.DefaultPageSettings.PrinterSettings.Copies = 1
+                    doc.PrinterSettings.Copies = 1
+                    Dim lsFlag As String = "2"
+                    If Not doc.PrinterSettings.IsValid Then
+                        lsFlag = "1"
+                    End If
+                    doc = Nothing
+                    GC.Collect()
+
+                    '打印
+                    Try
+                        oExcelApp.Run("Sheet1.FindPrinter", lsU_Printer, lsU_PsizeID, lsFlag)
+                        oExcelApp.ScreenUpdating = False
+                        oExcelApp.Run("GetDataString", Convert.ToString(liDocEntry))
+                        oExcelApp.ScreenUpdating = True
+
+                        m_objSheet.PrintOutEx()
+
+                        '打印完之后将打印日志插入到日志表
+                        'Dim lsInsertSQL As String = "Insert into PrintLog (TransType,Objtype,DocEntry,PrintDate,PrintUser,TemplateName) select '112','18','" + liDocEntry.ToString() + "','" + Format(DateTime.Now, "yyyy/MM/dd hh:mm:ss") + "','" + MyApplication.Company.UserName.Trim + "','" + lsTempName + "'"
+                        'ioTempSql.ExecuteQuery(lsInsertSQL)
+
+                    Catch ex As Exception
+                        MyApplication.SetStatusBarMessage(ex.ToString())
+                        BubbleEvent = False
+                        '   File.Delete(lsTargetFile)
+                        ' Return
+                    Finally
+                        m_objBook.Close()
+                        Dim deadProcess As Process = Process.GetProcessById(processid)  '获取该进程
+                        oExcelApp.Quit()
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(oExcelApp)
+                        oExcelApp = Nothing
+                        GC.Collect()
+                        deadProcess.Kill()  '杀死进程
+                    End Try
+
+                End If
+        End Select
+    End Sub
+End Class
